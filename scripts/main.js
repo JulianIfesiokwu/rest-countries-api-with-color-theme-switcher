b0ae98d9c028ce6650c52df540f47aace7430fff
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2021-08-04 10:45:59 +0100

    completed search by name functionality

---
+++ scripts/main.js
@@ -0,0 +1,106 @@
+const searchField = document.querySelector('.search__field');
+const resultsDiv = document.querySelector('.results');
+
+
+function thousands_separators(num) {
+    let num_parts = num.toString();
+    num_parts = num_parts.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return num_parts;
+}
+
+function showCountries(data) {
+    data.forEach((country) => {
+        displayCountry(country)
+    })
+    
+}
+
+const showSearchResults = async () => {
+    try {
+        const url = `https://restcountries.eu/rest/v2/all`
+        const result = await fetch(url)
+        const allCountries = await result.json()
+        //search data based on client input
+        refineResults(allCountries)
+    }
+    catch(err) {
+        console.log(err)
+    }
+}
+
+function refineResults(allCountries) {
+    let searchTerm = searchField.value;
+    // Clear results container of previous searches
+    resultsDiv.innerHTML = '';
+    // Loop through country list and display only countries that meet the search term
+    allCountries.forEach((country) => {
+        const countryName = (country.name).toLowerCase();
+        // console.log('no problem')
+        if(countryName.indexOf(searchTerm) !=-1) {
+            console.log('yes')
+            displayCountry(country)
+        } 
+    })
+}
+
+function displayCountry(country) {        
+    let countryCard = document.createElement('div');
+    countryCard.classList.add('country__card');
+    //create div for flag
+    let countryFlag = document.createElement('div');
+    countryFlag.classList.add('country__flag');
+    //create img element for flag
+    let flagContainer = document.createElement('span');
+    flagContainer.classList.add('country__img');
+    // Create img element and add flag
+    let imgContainer = document.createElement('img');
+    imgContainer.setAttribute('src', `${country.flag}`);
+    imgContainer.setAttribute('alt', `${country.name}`);       
+    //Create div for country information
+    let countryInformation = document.createElement('div');
+    countryInformation.classList.add('country__information');
+    // Create h2 for title information
+    let h2 = document.createElement('h2');
+    h2.classList.add('country__name');
+    h2.textContent = `${country.name}`;
+    // Create p element for population
+    let countryPopulation = document.createElement('p');
+    countryPopulation.classList.add('country__population');
+    countryPopulation.textContent = `Population: ${thousands_separators(country.population)}`;
+    // Create p element for continent
+    let countryContinent = document.createElement('p');
+    countryContinent.classList.add('country__continent');
+    countryContinent.textContent = `Region: ${country.region}`;
+    // Create p element for capital
+    let countryCapital = document.createElement('p');
+    countryCapital.classList.add('country__continent');
+    countryCapital.textContent = `Capital: ${country.capital}`;
+    // Append all information to card
+    flagContainer.appendChild(imgContainer)
+    countryFlag.appendChild(flagContainer)
+    countryCard.appendChild(countryFlag)
+
+    countryInformation.append(h2, countryPopulation, countryContinent, countryCapital)
+    countryCard.appendChild(countryInformation)
+
+    // Add to DOM
+    resultsDiv.appendChild(countryCard)
+}
+
+const getCountries = async () => {
+    try {
+        const url = `https://restcountries.eu/rest/v2/all`
+        const result = await fetch(url)
+        const data = await result.json()
+        // display results
+        showCountries(data)        
+    }
+    catch(err) {
+        console.log(err)
+    }
+}
+
+// displays all countries on load
+// getCountries();
+
+searchField.addEventListener('input', showSearchResults)
