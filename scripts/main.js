bbaec6b73657bfc5b3469e5e0a9ac24da94c875c
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2021-08-05 14:39:59 +0100

    have achieved all functionality using javascript, styling of dark mode and mobile view of span elements remaining

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,13 @@
 const searchField = document.querySelector('.search__field');
 const resultsDiv = document.querySelector('.results');
 const selectedRegion = document.querySelector('#continents');
+const countryCard = document.querySelector('.country__card');
+const search = document.querySelector('.search');
+const detailedInfo = document.querySelector('.detailed-info');
+const switchTheme = document.querySelector('.switch-theme');
+const countryContent = document.querySelector('.country__content');
+const backBtn = document.querySelector('.back');
+const borderCountryList = document.querySelector('.border__list');
 
 // Seperate population value by thousands
 function thousands_separators(num) {
@@ -39,7 +46,6 @@ function refineResults(allCountries) {
     // Loop through country list and display only countries that meet the search term
     allCountries.forEach((country) => {
         const countryName = (country.name).toLowerCase();
-        // console.log('no problem')
         if(countryName.indexOf(searchTerm) !=-1) {
             displayCountry(country)
         } 
@@ -60,16 +66,92 @@ const showRegionCountries = async () => {
             // Clear previous results
             resultsDiv.innerHTML = '';
             // display results based on region
-
             regionData.forEach((country) => {
                 displayCountry(country)
-            })
-            // showCountries(data)        
+            })      
         }
         catch(err) {
             console.log(err)
         }
     }
+}
+
+function getDetailedInformation(e) {
+    let target = e.target.closest('.country__card');
+    let targetCountry = target.firstElementChild.nextSibling.firstElementChild.textContent.toLowerCase();
+    // Hide search results div
+    resultsDiv.style.display = 'none';
+    search.style.display = 'none';
+    detailedInfo.style.display = 'block';
+    // display detailed information about selected country
+    showDetailedInfo(targetCountry);
+}
+
+const showDetailedInfo = async (targetCountry) => {
+    // Clear previous results
+    // countryContent.innerHTML = '';
+    try {
+        const url = `https://restcountries.eu/rest/v2/name/${targetCountry}`
+        const result = await fetch(url)
+        const countryData = await result.json()
+        // display results based on targetCountry
+        showDetailedCountry(countryData)   
+    }
+    catch(err) {
+        console.log(err)
+    }
+}
+
+function showDetailedCountry(countryData) {   
+    const imgSpan = document.querySelector('.detailed__country__img');
+    let imgContainer = document.createElement('img');
+    imgContainer.setAttribute('src', `${countryData[0].flag}`);
+    imgContainer.setAttribute('alt', `${countryData[0].name}`);
+    imgSpan.appendChild(imgContainer)
+    console.log(countryData[0])
+    const countryName = document.querySelector('.country__name_2');
+    countryName.textContent=`${countryData[0].name}`;
+    const nativeName = document.querySelector('.native__name');
+    nativeName.textContent = `${countryData[0].nativeName}`;
+    const population = document.querySelector('.population');
+    population.textContent = `${thousands_separators(countryData[0].population)}`;
+    const continent = document.querySelector('.continent');
+    continent.textContent = `${countryData[0].region}`;
+    const subRegion = document.querySelector('.sub-region');
+    subRegion.textContent = `${countryData[0].subregion}`;
+    const capital = document.querySelector('.capital');
+    capital.textContent = `${countryData[0].capital}`;
+    const domain = document.querySelector('.domain');
+    domain.textContent = `${countryData[0].topLevelDomain}`;
+    const currencies = document.querySelector('.currencies');
+    currencies.textContent = `${countryData[0].currencies[0].name}`;
+    // Extract language data
+    let languagesArray = [];
+    countryData[0].languages.forEach((language) => {
+        languagesArray.push(language.name)
+        });
+    // Convert to string
+    let languagesString = languagesArray.toString();
+    const languages = document.querySelector('.languages');
+    languages.textContent = `${languagesString}`;
+    // Get border countries
+    let borderCountries = countryData[0].borders;
+    // Loop through each border country code and find country name
+    borderCountries.forEach(async (borderCountryCode) => {
+        try {
+            const url = `https://restcountries.eu/rest/v2/alpha/${borderCountryCode}`;
+            const result = await fetch(url);
+            const countryName = await result.json()
+            // Create span element
+            let borderCountry = document.createElement('span');
+            borderCountry.textContent=`${countryName.name}`;
+            borderCountry.classList.add('border--countries');
+            borderCountryList.append(borderCountry)
+        } catch(err) {
+            console.error(err)
+        }
+    });
+    
 }
 
 // Display a country
@@ -112,7 +194,6 @@ function displayCountry(country) {
 
     countryInformation.append(h2, countryPopulation, countryContinent, countryCapital)
     countryCard.appendChild(countryInformation)
-
     // Add to DOM
     resultsDiv.appendChild(countryCard)
 }
@@ -131,8 +212,30 @@ const getCountries = async () => {
     }
 }
 
+function changeTheme(e) {
+    console.log(e)
+}
+
+function goBack() {
+    resultsDiv.style.display='flex';
+    detailedInfo.style.display='none';
+    search.style.display = 'flex';
+    const imgSpan = document.querySelector('.detailed__country__img');
+    // remove previous flags if any
+    while (imgSpan.hasChildNodes()) {  
+        imgSpan.removeChild(imgSpan.firstChild);
+    }
+    // remove previous list
+    while (borderCountryList.hasChildNodes()) {
+        borderCountryList.removeChild(borderCountryList.firstChild)
+    }
+}
+
 // displays all countries on load
 getCountries();
 
 searchField.addEventListener('input', showSearchResults)
 selectedRegion.addEventListener('input', showRegionCountries)
+resultsDiv.addEventListener('click', getDetailedInformation)
+switchTheme.addEventListener('click', changeTheme);
+backBtn.addEventListener('click', goBack)
